2d7de7dcdf5b128e720fc9b2c31ad201a3f2a296
RyuMaster <assignators@gmail.com>
2021-04-28 15:27:50 +0300

    Update to compile against latest msys2 mingw64

--- libxayagame/Compile-scripts/build.sh
+++ libxayagame/Compile-scripts/build.sh
@@ -1,5 +1,7 @@
+https://github.com/xaya/XAYA_tutorial_code.git
 pacman -S base-devel git mingw-w64-x86_64-toolchain make mingw-w64-x86_64-cmake autoconf-archive mingw-w64-x86_64-protobuf mingw-w64-x86_64-gtest mingw-w64-x86_64-gflags mingw-w64-x86_64-zeromq mingw-w64-x86_64-openssl mingw-w64-x86_64-glog mingw-w64-x86_64-lmdb mingw-w64-x86_64-lmdbxx mingw-w64-x86_64-sqlite3 mingw-w64-x86_64-libmicrohttpd wget
-git clone https://github.com/jonathanmarvens/argtable2.git
+pacman -U http://repo.msys2.org/mingw/x86_64/mingw-w64-x86_64-jsoncpp-1.9.2-1-any.pkg.tar.xz
+git clone https://github.com/RyuMaster/argtable2.git
 cd argtable2
 cmake . -G"MSYS Makefiles" -DCMAKE_INSTALL_PREFIX=$MINGW_PREFIX
 make -j2
@@ -11,7 +13,7 @@ cd libjson-rpc-cpp
 git checkout listen-locally-0.7.0
 mkdir win32-deps
 mkdir win32-deps/include
-cmake . -G"MSYS Makefiles" -DCMAKE_INSTALL_PREFIX=$MINGW_PREFIX -DREDIS_SERVER=NO -DREDIS_CLIENT=NO  -DCOMPILE_STUBGEN=YES -DCOMPILE_EXAMPLES=NO -DCOMPILE_TESTS=NO -DBUILD_STATIC_LIBS=YES -DHUNTER_ENABLED=YES
+cmake . -G"MSYS Makefiles" -DCMAKE_INSTALL_PREFIX=$MINGW_PREFIX -DREDIS_SERVER=NO -DREDIS_CLIENT=NO  -DCOMPILE_STUBGEN=YES -DCOMPILE_EXAMPLES=NO -DCOMPILE_TESTS=NO -DBUILD_STATIC_LIBS=YES -DHUNTER_ENABLED=YES -DCMAKE_CXX_FLAGS="-fpermissive"
 make -j2
 make install
 cd dist
